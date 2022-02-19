52c18de75bf5b4cfe9824f7ebbcad179fd985196
Alan Choi <alan.choi@sendbird.com>
2022-02-19 19:36:43 +0900

    chore: add sentry

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import '@formatjs/intl-datetimeformat/add-all-tz';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './GlobalStyles';
 import './index.css';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
